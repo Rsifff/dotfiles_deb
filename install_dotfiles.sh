a11716149589e097a16517f16efee4cd4387f445
Rsifff <rsifff@KALIWIN.localdomain>
2020-09-22 18:22:59 +0200

    maj

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -21,7 +21,7 @@ function update() {
 
 	function apt_packages() {
 	colorecho "[+] Installing APT packages"
-	apt install -y curl zsh python3-pip git tmux neofetch wget rubygems build-essential
+	apt install -y rubygems curl zsh python3-pip git tmux neofetch  
     	gem install tmuxinator	
 }
 
